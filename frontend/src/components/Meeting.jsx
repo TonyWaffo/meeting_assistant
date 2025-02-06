9d7c5ad97f4e13ad8de7b8b1054da08aacb52e6a
Tony Waffo <waffotony@gmail.com>
2025-02-06 14:01:53 -0500

    query container for the chat feature is updated for better user experience

--- frontend/src/components/Meeting.jsx
+++ frontend/src/components/Meeting.jsx
@@ -7,18 +7,11 @@ import { IoSendSharp } from "react-icons/io5";
 
 const Meeting=()=>{
     const [file,setFile]=useState("");
-    const [transcript,setTranscript]=useState("hh");
+    const [transcript,setTranscript]=useState("jgh");
     const [conversationHistory,setConversationHistory]=useState(null);
+    const [loadingResponse,setLoadingResponse]=useState("");
 
     const queryTextAreaRef=useRef(null);
-
-    const handleInputChange=(event)=>{
-        const textArea=queryTextAreaRef.current;
-
-        if(event.target.value.trim()==""){
-            textArea.style.height="80px";
-        }
-    }
 
     let conversations=[
         {
@@ -52,30 +45,51 @@ const Meeting=()=>{
             content:"........"
         },
     ]
+
+    const handleInputChange=(event)=>{
+        const textArea=queryTextAreaRef.current;
+
+        if(event.target.value.trim()==""){
+            textArea.style.height="200px";
+        }
+    }
+
+    /**
+     * Create a useEffect hooks that upload the meeting content based on the
+     * meeting history(or not).
+     * Meaning if a new meeting is created the component will be fresh
+     * Might use axios to call the function: if a new meeting  is created
+     * , conversation, file, transcript and loading response are null, else
+     * set accordingly, use the hooks to update the conversation for persistence
+     * 
+     */
+
     return (
       <>
           <section className="meeting-section">
+            <div className='meeting-wrapper'>
             <h2>Your meeting assistant</h2>
-            {/*This part is ony viewable when the user create a new recording, not available in history and if the audio hasn't been processed yet*/}
-            <MediaHandler/>
+                {/*This part is ony viewable when the user create a new recording, not available in history and if the audio hasn't been processed yet*/}
+                <MediaHandler/>
 
-            {/* Only available when audio file is selected and trancribed button is pressed and when summary or text present in the history */}
-            <div className="meeting-content">
-                {/* if no transcript and no transcript */}
-                {(!transcript && !conversationHistory) && <p>Process your file upload or meeting record </p>}
+                {/* Only available when audio file is selected and trancribed button is pressed and when summary or text present in the history */}
+                <div className="meeting-content">
+                    {/* if no transcript and no transcript */}
+                    {(!transcript && !conversationHistory) && <p>Process your file upload or meeting record </p>}
 
-                {/* if there is a file or histiry being processed, implement a spinning animation */}
-                
+                    {/* if there is a file or histiry being processed, implement a spinning animation */}
+                    
 
-                {/* If there is either a transcript available or a conversation history */}
-                { (transcript || conversationHistory) && conversations.map((conversation,index)=>(
-                    <div className={`conversation ${conversation.speaker=="system" ? "system":"user"}`}>
-                        <span>{conversation.speaker}</span>
-                        <b>{conversation.subject}</b>
-                        <p>{conversation.content}</p>
-                    </div>
-                ))}
+                    {/* If there is either a transcript available or a conversation history */}
+                    { (transcript || conversationHistory) && conversations.map((conversation,index)=>(
+                        <div className={`conversation ${conversation.speaker=="system" ? "system":"user"}`}>
+                            <span>{conversation.speaker}</span>
+                            <b>{conversation.subject}</b>
+                            <p>{conversation.content}</p>
+                        </div>
+                    ))}
 
+                </div>
             </div>
 
 
@@ -83,27 +97,35 @@ const Meeting=()=>{
                 <textarea className="query-input" 
                     ref={queryTextAreaRef} 
                     onChange={handleInputChange}
-                    placeholder='Posez une questions sur le meeting'>
+                    placeholder='Posez une question sur le meeting'>
 
                 </textarea>
 
                 <div className="query-actions">
                     {/* Hide this part when the response is loading */}
-                    {/* Gray out these elements when no audio file is available  */}
-                    <div className='query-buttons'>
-                        <div>
-                            <button>Transcription</button>
-                            <button>Resumer</button>
+                    {/* Gray out these elements when no transcript is available  */}
+                    {!loadingResponse && 
+                        <div className='query-buttons'>
+                            <div>
+                                <button>Transcription</button>
+                                <button>Resumer</button>
+                            </div>
+                            <div>
+                                <IoSendSharp size={30} /> {/* sending icon is grayed out if the text area is empty or the response is loading */}
+                            </div>
                         </div>
-                        <div>
-                            <IoSendSharp size={30} /> {/* sending icon is grayed out if the text area is empty or the response is loading */}
-                        </div>
-                    </div>
+                    }
 
                     {/* Create an animation when the response is loading  */}
+
+
+                    {loadingResponse && 
                         <div className="query-loading">
-                            ...
+                            <span></span>
+                            <span></span>
+                            <span></span>
                         </div>
+                    }
                 </div>
             </div>
           </section>
